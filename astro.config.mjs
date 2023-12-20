a6bd6b44022378b1cdbd9f314e572071cdfe3853
Blede <bledegraphics@gmail.com>
2023-12-19 21:28:19 -0500

    Basic i18n and submodule setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [react(), astroI18next()],
+  adapter: node({
+    mode: "standalone"
+  }),
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            // force .json imports into their own chunks
+            // so that they aren't inadvertently redownloaded
+            if (id.endsWith(".json")) {
+              const { base } = path.parse(id);
+              return base;
+            }
+            return;
+          },
+        },
+      },
+    },
+  },
+});
