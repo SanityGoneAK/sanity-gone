5da64e41a43ebbfa0ed9c911dbc8eb0ea548ad65
Ian Kim <me@ian.kim>
2024-01-08 20:47:15 -0500

    Add missing path import
    Only broke on astro build, so we missed it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import path from "path";
 import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
 import node from "@astrojs/node";
