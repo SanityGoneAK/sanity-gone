bf30efc69a52a6aabae26af74c151b2172b4026a
Blede <bledegraphics@gmail.com>
2024-01-20 15:07:51 -0500

    WIP Machine Translations with AI

---
+++ scripts/translate-operators.js
@@ -0,0 +1,70 @@
+import OpenAI from "openai";
+
+/**
+ * @param {"zh_CN" | "en_US" | "ja_JP" | "ko_KR"} locale - output locale
+ */
+export async function translateOperators(locale) {
+	if (locale === "zh_CN") {
+		return;
+	}
+
+	const operators = Object.entries(
+		await import(`../data/${locale}/operators.json`)
+	).filter(
+		([charId, operator]) =>
+			operator.serverLocales &&
+			operator.serverLocales.length === 1 &&
+			operator.serverLocales.some(
+				(serverLocale) => serverLocale === "zh_CN"
+			)
+	);
+
+	console.log(
+		(await translateString(locale, operators[1][1].description)).choices[0]
+			.message.content
+	);
+}
+
+function translateString(locale, string) {
+	const localeMap = {
+		en_US: "English",
+		ja_JP: "Japanese",
+		ko_KR: "Korean",
+	};
+
+	const localeExamples = {
+		en_US: "Restores the HP of allied units and recovers <@ba.dt.element>Elemental Damage</> by 50% of ATK (can recover <@ba.dt.element>Elemental Damage</> of unhurt allied units)",
+		ja_JP: "味方のHPを回復し、同時に対象に蓄積された<@ba.dt.element>元素損傷</>を自身の攻撃力の50％の値分治療する（HP最大の味方も<@ba.dt.element>元素損傷</>の治療対象となる）",
+		ko_KR: "아군 유닛의 HP 회복, 동시에 공격력의 50%에 해당하는 <@ba.dt.element>원소 피해</> 회복 (부상이 없는 아군 유닛의 <@ba.dt.element>원소 피해</>도 회복 가능)",
+	};
+
+	const openai = new OpenAI({
+		baseURL: "http://localhost:1234/v1",
+		apiKey: "not-needed",
+	});
+	console.log(locale);
+	console.log(string);
+
+	return openai.chat.completions.create({
+		messages: [
+			{
+				role: "system",
+				content: `You are an accurate translator of content for a game. Some of the content in game has specific formats, you will preserve this format while also translating the text accurately.
+                Translate ALWAYS From Chinese to ${localeMap[locale]}. DO NOT UNDER ANY CIRUMSTANCE add anything to the response that is not the translation of the text given.
+                Translations should be in a Informal or Convertational style
+				
+                Some examples of translations are 
+				FROM: 恢复友方单位生命，并回复相当于攻击力50%的<@ba.dt.element>元素损伤</>（可以回复未受伤友方单位的<@ba.dt.element>元素损伤</>）to
+				TO: ${localeExamples[locale]}
+                
+                When responding do not include any formatting just the translated text`,
+			},
+			{
+				role: "user",
+				content: `${string}`,
+			},
+		],
+	});
+}
+
+translateOperators("en_US");
