51fdebc804717edaa1867bb8415420d812788270
Blede <bledegraphics@gmail.com>
2025-01-14 20:24:42 -0500

    Resolve class names

--- src/components/operator/OperatorBranchAndTrait.tsx
+++ src/components/operator/OperatorBranchAndTrait.tsx
@@ -6,9 +6,14 @@ import { subProfessionIdToBranch } from "~/utils/branches.ts";
 import { operatorBranchIcon } from "~/utils/images.ts";
 
 import TraitInfo from "./TraitInfo";
+import { localeStore } from "~/pages/[locale]/_store.ts";
+import { useTranslations } from "~/i18n/utils.ts";
+import type { ui } from "~/i18n/ui.ts";
+import { type Locale, localeToTag } from "~/i18n/languages.ts";
 
 const OperatorBranchAndTrait: React.FC = () => {
 	const operator = useStore(operatorStore);
+	const locale = useStore(localeStore) as Locale;
 	return (
 		<div className="grid grid-flow-col items-center gap-x-2">
 			<img
@@ -25,7 +30,7 @@ const OperatorBranchAndTrait: React.FC = () => {
 					className="relative cursor-help after:absolute after:bottom-0 after:left-0 after:right-0
 							   after:border-b after:border-dashed after:opacity-[33%]"
 				>
-					{subProfessionIdToBranch(operator.subProfessionId)}
+					{subProfessionIdToBranch(operator.subProfessionId, localeToTag[locale])}
 				</span>
 			</Tooltip>
 		</div>
--- src/components/operator/TraitInfo.tsx
+++ src/components/operator/TraitInfo.tsx
@@ -1,12 +1,26 @@
-import branchesJson from "../../../data/branches.json";
+import enBranchesJson from "../../../data/en_US/branches.json";
+import krBranchesJson from "../../../data/ko_KR/branches.json";
+import cnBranchesJson from "../../../data/zh_CN/branches.json";
+import jpBranchesJson from "../../../data/ja_JP/branches.json";
+import { useStore } from "@nanostores/react";
+import { localeStore } from "~/pages/[locale]/_store.ts";
+import type { Locale } from "~/i18n/languages.ts";
 
 interface Props {
 	subProfessionId: string;
 }
 
 const TraitInfo: React.FC<Props> = ({ subProfessionId }) => {
+	const locale = useStore(localeStore);
+	const branchMap = {
+		en: enBranchesJson,
+		kr: krBranchesJson,
+		'zh-cn': cnBranchesJson,
+		jp: jpBranchesJson,
+	}
+	const branchesJson = branchMap[locale as keyof typeof branchMap];
 	const { trait } =
 		branchesJson[subProfessionId as keyof typeof branchesJson];
-	return <span dangerouslySetInnerHTML={{ __html: trait.en_US }} />;
+	return <span dangerouslySetInnerHTML={{ __html: trait }} />;
 };
 export default TraitInfo;
