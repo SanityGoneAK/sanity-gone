5c68348d97e9b99b5fbf3fa89dc837dbfd8a2526
Blede <bledegraphics@gmail.com>
2025-01-18 23:40:08 -0500

    Static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "static",
 	i18n: {
 		defaultLocale: "en",
 		locales: [
