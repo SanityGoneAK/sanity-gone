96964a5e0950950b83b4a6ad67a6283156a0b6f0
Blede <bledegraphics@gmail.com>
2025-04-26 07:07:10 -0400

    Add submodules to commits

--- scripts/axios.ts
+++ scripts/axios.ts
@@ -4,7 +4,7 @@ const customAxiosInstance = axios.create({
 	timeout: 60000,
 	headers: {
 		"User-Agent":
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.1.0.0 Safari/537.36",
 		"Cache-Control": "no-store",
 		Pragma: "no-cache",
 		Expires: "0",
