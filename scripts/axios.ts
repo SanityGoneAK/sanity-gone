816f71eb8a9846641bf511753d1d79d69a0ebede
Blede <bledegraphics@gmail.com>
2025-10-09 07:44:49 -0400

    Game update Mantra

--- scripts/axios.ts
+++ scripts/axios.ts
@@ -4,7 +4,7 @@ const customAxiosInstance = axios.create({
 	timeout: 60000,
 	headers: {
 		"User-Agent":
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.479 (KHTML, like Gecko) Version/12.1.1 Safari/602.1.84",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.479 (KHTML, like Gecko) Version/12.1.1 Safari/601.1.84",
 		// "Cache-Control": "no-store",
 		// Pragma: "no-cache",
 		// Expires: "0",
