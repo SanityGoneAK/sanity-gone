c00129b79b6e086a53a347ba86eb0e8b9c942eea
Blede <bledegraphics@gmail.com>
2026-02-05 21:59:44 -0500

    react fix

--- apps/sanity-gone/astro.config.mjs
+++ apps/sanity-gone/astro.config.mjs
@@ -67,6 +67,13 @@ export default defineConfig({
                 },
             },
         },
+        resolve: {
+            // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+            // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+            alias: import.meta.env.PROD && {
+              "react-dom/server": "react-dom/server.edge",
+            },
+          },
         // vite warns if this option isn't present for some reason
         // css: {
         // 	postcss: {
